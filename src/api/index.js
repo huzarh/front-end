66f6bf84550f7ed7851ddb66d490ebc14b399e6d
khuzar <huzarh44@gmail.com>
2023-02-25 23:43:34 +0300

    axios soilson

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "http://64.226.76.186:5000/api/v1",
 });
 // .defaults.withCredentials = true
