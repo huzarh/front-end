760729ac361882c3d9b9dbefe89b27947908eb32
huzarh <huzarh44@gmail.com>
2023-06-12 00:21:00 +0300

    hafta son

--- src/Page/getStarted/index.js
+++ src/Page/getStarted/index.js
@@ -9,7 +9,7 @@ const GetStarted = () => {
       <img className={css.Logo} src={Image} alt="logo" />
       <div className={css.gol}>
         <b className={css.text}>
-          <h1 className={css.h1}>Yardımcı</h1>
+          <h1 className={css.h1}>Türkçe Öğretimi</h1>
           <p className={css.pa}>
             İstediğiniz zaman ve istediğiniz yerde dil öğrenin. Ücretsiz ve
             sonsuza kadar.
